223a15985d85f70b2a4afbdc57105bb1aa6c9152
Felipe Visoski Dos Santos <136396397+Felipe-Visoski@users.noreply.github.com>
2023-11-14 10:47:09 -0300

    14-11

--- GeradorCNPJ/geradorcnpj.js
+++ GeradorCNPJ/geradorcnpj.js
@@ -1,29 +1,33 @@
-function gerarCNPJ(ponto){
-    const n= 9;
-    const n1 = Math.floor(Math.random() * n) + 1;
-    const n2 = Math.floor(Math.random() * n); 
-    const n3 = Math.floor(Math.random() * n);
-    const n4 = Math.floor(Math.random() * n);
-    const n5 = Math.floor(Math.random() * n);
-    const n6 = Math.floor(Math.random() * n);
-    const n7 = Math.floor(Math.random() * n);
-    const n8 = Math.floor(Math.random() * n);
-    const n9 = 0; // O nono dígito é sempre 0 no início
-    const n10 = 0; // O décimo dígito é sempre 0 no início
-    const n11 = 0; // O décimo primeiro dígito é sempre 0 no início
-    const n12 = 1; // O décimo segundo dígito é sempre 1 no início
-
-    const digito1 = (n12 * 2 + n11 * 3 + n10 * 4 + n9 * 5 + n8 * 6 + n7 * 7 + n6 * 8 + n5 * 9 + n4 * 2 + n3 * 3 + n2 * 4 + n1 * 5) % 11;
-    const digito2 = (n12 * 2 + digito1 * 3 + n10 * 4 + n9 * 5 + n8 * 6 + n7 * 7 + n6 * 8 + n5 * 9 + n4 * 2 + n3 * 3 + n2 * 4 + n1 * 5) % 11;
-
+function gerarCNPJ(ponto) {
+    var n = 9;
+    var n1 = Math.round(Math.random()*n);
+    var n2 = Math.round(Math.random()*n);
+    var n3 = Math.round(Math.random()*n);
+    var n4 = Math.round(Math.random()*n);
+    var n5 = Math.round(Math.random()*n);
+    var n6 = Math.round(Math.random()*n);
+    var n7 = Math.round(Math.random()*n);
+    var n8 = Math.round(Math.random()*n);
+    var n9 = 0;//Math.round(Math.random()*n);
+    var n10 = 0;//Math.round(Math.random()*n);
+    var n11 = 0;//Math.round(Math.random()*n);	
+    var n12 = 1;//Math.round(Math.random()*n);
+    var d1 = n12*2+n11*3+n10*4+n9*5+n8*6+n7*7+n6*8+n5*9+n4*2+n3*3+n2*4+n1*5;
+    d1 = 11 - ( mod(d1,11) );
+    if (d1>=10) d1 = 0;
+    var d2 = d1*2+n12*3+n11*4+n10*5+n9*6+n8*7+n7*8+n6*9+n5*2+n4*3+n3*4+n2*5+n1*6;
+    d2 = 11 - ( mod(d2,11) );
+    if (d2>=10) d2 = 0;
     if(ponto){
-    const cnpj = `${n1}${n2}.${n3}${n4}${n5}.${n6}${n7}${n8}/${n9}${n10}${n11}${n12}-${digito1}${digito2}`;
-    return cnpj;
-    }else{
-        const cnpj = `${n1}${n2}${n3}${n4}${n5}${n6}${n7}${n8}${n9}${n10}${n11}${n12}${digito1}${digito2}`;
-        return cnpj;
-    }
+    return ''+n1+n2+'.'+n3+n4+n5+'.'+n6+n7+n8+'/0001-'+d1+d2;
+    }else {
+        return ''+n1+n2+n3+n4+n5+n6+n7+n8+'0001'+d1+d2;
     
+    }
+}
+
+function mod(dividendo,divisor) {
+    return Math.round(dividendo - (Math.floor(dividendo/divisor)*divisor));
 }
 
 
--- GeradorSenha/geradorsenha.js
+++ GeradorSenha/geradorsenha.js
@@ -1,29 +1,18 @@
 function gerarSenha() {
-    const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const senhaArray = [];
-
-    // Adiciona pelo menos 1 letra maiúscula
-    senhaArray.push(caracteres.charAt(Math.floor(Math.random() * 26)));
-
-    // Adiciona pelo menos 1 letra minúscula
-    senhaArray.push(caracteres.charAt(Math.floor(Math.random() * 26) + 26));
-
-    // Adiciona pelo menos 2 números
-    senhaArray.push(caracteres.charAt(Math.floor(Math.random() * 10)));
-    senhaArray.push(caracteres.charAt(Math.floor(Math.random() * 10)));
-
-    // Adiciona caracteres restantes
-    for (let i = senhaArray.length; i < 9; i++) {
-        senhaArray.push(caracteres.charAt(Math.floor(Math.random() * caracteres.length)));
+    var length = 9,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%&",
+        retVal = "";
+    var upperCaseChar = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    var lowerCaseChar = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    var specialChar = "@#$%&".charAt(Math.floor(Math.random() * 5));
+    var numberChar1 = String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    var numberChar2 = String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    var passwordPrefix = upperCaseChar + lowerCaseChar + specialChar + numberChar1 + numberChar2;
+    for (var i = 0, n = charset.length; i < length - 5; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
     }
-
-    // Embaralha a senha
-    senhaArray.sort(() => Math.random() - 0.5);
-
-    // Converte o array para string
-    const senha = senhaArray.join('');
-    
-    return senha;
+    retVal = passwordPrefix + retVal;
+    return retVal;
 }
 
 
